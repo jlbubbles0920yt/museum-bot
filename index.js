251e2544d83b333f674cc06a65747d997e53d19f
Jose Moran Urena <jlbubbles0920@Joses-MacBook-Air.local>
2020-09-11 17:04:14 -0400

    make it better

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "bm?";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}! ${client.user.id}`);
+  client.user.setActivity("I am not ready yet. Use bm!help!")
+});
+
+client.on('message', message => {
+  if(message.content.startsWith(`${prefix}help`)) {
+    message.channel.send("I am not ready yet! Wait for my developer to give me stuff to do!")
+  }
+});
+
+client.login('NzU0MDc1MDEwNTE5NTMxNjAx.X1vdFA.IvYfzjxgNgKN4XhveioSMxFttwI');
