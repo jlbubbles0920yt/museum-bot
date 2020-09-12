2b45c889431e6979bbc6026e2225b4a2e77b8b8e
Jose Moran Urena <jlbubbles0920@Joses-MacBook-Air.local>
2020-09-12 10:10:42 -0400

    make it better

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "bm?";
 
+const { GiveawaysManager } = require("discord-giveaways");
+
+const manager = new GiveawaysManager(client, {
+    storage: "./giveaways.json",
+    updateCountdownEvery: 10000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: [ "MANAGE_MESSAGES", "ADMINISTRATOR" ],
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
+
+client.giveawaysManager = manager;
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}! ${client.user.id}`);
   client.user.setActivity("use bm?help!")
@@ -10,7 +25,8 @@ client.on('ready', () => {
 client.on('message', message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  const args = message.content.slice(prefix.length).trim().split(' ');
+  const ms = require("ms");
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
 
   if(message.content.startsWith(`${prefix}help`)) {
     message.channel.send(`Commands are:\n${prefix}say\nThats all!`)
